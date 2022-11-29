6bed6ac125a13427209da9a3a623d963dbdae58c
endeavouros-arm <pudge@endeavouros.com>
2022-11-29 11:52:52 -0700

    Update all with reworked files

--- build-server-image-chroot.sh
+++ build-server-image-chroot.sh
@@ -18,133 +18,27 @@ _check_internet_connection() {
     fi
 }   # end of function _check_internet_connection
 
-_find_mirrorlist() {
-    # find and install current endevouros-arm-mirrorlist
-    local tmpfile
-    local currentmirrorlist
-    local ARMARCH="aarch64"
-
-    printf "\n${CYAN}Find current endeavouros-mirrorlist...${NC}\n\n"
-    sleep 1
-    curl https://github.com/endeavouros-team/repo/tree/master/endeavouros/$ARMARCH | grep "endeavouros-mirrorlist" | sed s'/^.*endeavouros-mirrorlist/endeavouros-mirrorlist/'g | sed s'/pkg.tar.zst.*/pkg.tar.zst/'g | tail -1 > mirrors
-
-    tmpfile="mirrors"
-    read -d $'\x04' currentmirrorlist < "$tmpfile"
-
-    printf "\n${CYAN}Downloading endeavouros-mirrorlist...${NC}"
-    wget https://github.com/endeavouros-team/repo/raw/master/endeavouros/$ARMARCH/$currentmirrorlist 2>> /root/enosARM.log
-
-    printf "\n${CYAN}Installing endeavouros-mirrorlist...${NC}\n"
-    pacman -U --noconfirm $currentmirrorlist
-
-    # printf "\n[sar]\nSigLevel = PackageRequired\nServer = http://127.0.0.1:22122\n\n" >> /etc/pacman.conf
-    printf "\n[endeavouros]\nSigLevel = PackageRequired\nInclude = /etc/pacman.d/endeavouros-mirrorlist\n\n" >> /etc/pacman.conf
-
-    rm mirrors
-}  # end of function _find_mirrorlist
-
-
-_find_keyring() {
-    local tmpfile
-    local currentkeyring
-    local ARMARCH="aarch64"
-
-    printf "\n${CYAN}Find current endeavouros-keyring...${NC}\n\n"
-    sleep 1
-    curl https://github.com/endeavouros-team/repo/tree/master/endeavouros/$ARMARCH | grep endeavouros-keyring | sed s'/^.*endeavouros-keyring/endeavouros-keyring/'g | sed s'/pkg.tar.zst.*/pkg.tar.zst/'g | tail -1 > keys
-
-    tmpfile="keys"
-    read -d $'\04' currentkeyring < "$tmpfile"
-
-    printf "\n${CYAN}Downloading endeavouros-keyring...${NC}"
-    wget https://github.com/endeavouros-team/repo/raw/master/endeavouros/$ARMARCH/$currentkeyring 2>> /root/enosARM.log
-
-    printf "\n${CYAN}Installing endeavouros-keyring...${NC}\n"
-    pacman -U --noconfirm $currentkeyring
-
-    rm keys
-}   # End of function _find_keyring
-
-_base_addons() {
-    ### the following installs all packages needed to match the EndeavourOS base install
-    printf "\n${CYAN}Installing EndeavourOS Base Addons...${NC}\n"
-    eos-packagelist --arch arm "Desktop-Base + Common packages" "Firefox and language package" > base-addons
-    printf "openbox\npcmanfm-gtk3\ntint2\nnetwork-manager-applet\nxfce4-terminal\nflameshot" >> base-addons
-    pacman -S --noconfirm --needed - < base-addons
-    ### Install Calamares Arm
-    pacman -U --noconfirm /home/alarm/configs/xkeyboard-config-2.35.1-1-any.pkg.tar.xz
-    pacman -S --noconfirm --needed calamares_current_arm calamares_config_default_arm calamares_config_ce_arm
-    rm -rf base-addons
-}   # end of function _base_addons
-
 _finish_up() {
-    case $OPSYSTEM in
-    Desktop) printf "alarm ALL=(ALL:ALL) NOPASSWD: ALL\n" >> /etc/sudoers
-        gpasswd -a alarm wheel
-        printf "exec openbox-session\n" >> /home/alarm/.xinitrc
-        printf "if [ -z \"\${DISPLAY}\" ] && [ \"\${XDG_VTNR}\" -eq 1 ]; then\n  exec startx\nfi\n" >> /home/alarm/.bash_profile
-        chown alarm:alarm /home/alarm/.xinitrc
-        chmod 644 /home/alarm/.xinitrc
-        systemctl disable dhcpcd.service
-        systemctl enable NetworkManager.service
-        pacman -Rn --noconfirm dhcpcd
-        cp /home/alarm/configs/ORION-sky-ARM.png /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
-        cp /home/alarm/configs/EOS-PLANETS-ARM.png /usr/share/endeavouros/backgrounds/endeavouros-calamares-wallpaper.png
-        rm -rf /endeavouros*
-        usermod -u 2001 alarm
-        groupmod -g 2001 alarm
-        cd /home/alarm/
-        rm -rf .config
-        mkdir .config
-        mkdir Desktop
-        cd configs/
-        # cp /boot/config.txt /boot/config.txt.orig
-        # cp rpi4-config.txt /boot/config.txt
-        cp /usr/lib/systemd/system/getty@.service /usr/lib/systemd/system/getty@.service.bak
-        cp getty@.service /usr/lib/systemd/system/getty@.service
-        cp clean-up.sh /usr/local/bin/clean-up.sh
-        chmod +x /usr/local/bin/clean-up.sh
-        cp clean-up.service /etc/systemd/system/clean-up.service
-       ./alarmconfig.sh
-       ./calamares.sh
-       cd ..
-       chown -R alarm .config Desktop .Xauthority
-       printf "[Match]\nName=wlan*\n\n[Network]\nDHCP=yes\nDNSSEC=no\n" > /etc/systemd/network/wlan.network
-       timedatectl set-ntp true
-       timedatectl timesync-status ;;
-    esac
     printf "\nalias ll='ls -l --color=auto'\n" >> /etc/bash.bashrc
     printf "alias la='ls -al --color=auto'\n" >> /etc/bash.bashrc
     printf "alias lb='lsblk -o NAME,FSTYPE,FSSIZE,LABEL,MOUNTPOINT'\n\n" >> /etc/bash.bashrc
+
+    sed -i 's|# Server = http://mirror.archlinuxarm.org/$arch/$repo| Server = http://mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
+    sed -i 's| Server = http://fl.us.mirror.archlinuxarm.org/$arch/$repo|# Server = http://fl.us.mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
+    sed -i 's| Server = http://il.us.mirror.archlinuxarm.org/$arch/$repo|# Server = http://il.us.mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
+
     rm /var/cache/pacman/pkg/*
-    rm /root/build-image-chroot.sh
-    rm /root/enosARM.log
+    rm /root/build-server-image-chroot.sh
+    rm /root/platformname
     rm -rf /etc/pacman.d/gnupg
+    rm /home/alarm/smb.conf
+    rm /home/alarm/server-addons
+    cp /home/alarm/config-server.service /etc/systemd/system/
+    systemctl enable config-server.service
+    rm /home/alarm/config-server.service
     printf "\n\n${CYAN}Your uSD is ready for creating an image.${NC}\n"
 }   # end of function _finish_up
 
-_switch_mirrors_local() {
-   sed -i 's|Server = http://mirror.archlinuxarm.org/$arch/$repo|#Server = http://mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
-   sed -i 's|Server|#Server|g' /etc/pacman.d/endeavouros-mirrorlist
-   echo "Server = http://127.0.0.1:21212" >> /etc/pacman.d/mirrorlist
-   echo "Server = http://10.42.0.1:9129/repo/archlinux_\$arch/\$repo" >> /etc/pacman.d/mirrorlist
-   echo "Server = http://10.42.0.1:9129/repo/endeavouros/\$repo/\$arch" >> /etc/pacman.d/endeavouros-mirrorlist
-   # echo "Server = https://github.com/endeavouros-arm/repo/raw/master/\$repo/\$arch" >> /etc/pacman.d/endeavouros-mirrorlist
-   # printf "\n[sar]\nSigLevel = PackageRequired\nServer = http://127.0.0.1:22122\n\n" >> /etc/pacman.conf
-
-}
-
-_switch_mirrors_back() {
-    sed -i 's|#Server = http://mirror.archlinuxarm.org/$arch/$repo|Server = http://mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
-    sed -i 's|Server = http://127.0.0.1:21212||g' /etc/pacman.d/mirrorlist
-    sed -i 's|Server = http://10.42.0.1:9129/repo/archlinux_$arch/$repo||g' /etc/pacman.d/mirrorlist
-    sed -i 's|Server = http://10.42.0.1:9129/repo/endeavouros/$repo/$arch||g' /etc/pacman.d/endeavouros-mirrorlist
-    # sed -i 's|Server = https://github.com/endeavouros-arm/repo/raw/master/$repo/$arch||g' /etc/pacman.d/endeavouros-mirrorlist
-    sed -i 's|#Server|Server|g' /etc/pacman.d/endeavouros-mirrorlist
-    sed -i 's|\[sar\]||g' /etc/pacman.conf
-    sed -i 's|SigLevel = PackageRequired||g' /etc/pacman.conf
-    sed -i 's|Server = http://127.0.0.1:22122||g' /etc/pacman.conf
-}
 
 ######################   Start of Script   #################################
 Main() {
@@ -163,32 +57,24 @@ Main() {
    # read in platformname passed by install-image-aarch64.sh
    file="/root/platformname"
    read -d $'\x04' PLATFORM_NAME < "$file"
-   file="/root/mirrors"
-   read -d $'\x04' LOCAL < "$file"
-   file="/root/operatingsystem"
-   read -d $'\x04' OPSYSTEM < "$file"
    _check_if_root
    _check_internet_connection
    sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
-   sed -i "s|#Color|Color\nILoveCandy|g" /etc/pacman.conf
-   sed -i "s|#VerbosePkgLists|VerbosePkgLists\nDisableDownloadTimeout|g" /etc/pacman.conf
+   sed -i 's|#Color|Color\nILoveCandy|g' /etc/pacman.conf
+   sed -i 's|#VerbosePkgLists|VerbosePkgLists\nDisableDownloadTimeout|g' /etc/pacman.conf
+   sed -i 's| Server = http://mirror.archlinuxarm.org/$arch/$repo|# Server = http://mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
+   sed -i 's|# Server = http://fl.us.mirror.archlinuxarm.org/$arch/$repo| Server = http://fl.us.mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
+   sed -i 's|# Server = http://il.us.mirror.archlinuxarm.org/$arch/$repo| Server = http://il.us.mirror.archlinuxarm.org/$arch/$repo|g' /etc/pacman.d/mirrorlist
+
    pacman-key --init
    pacman-key --populate archlinuxarm
    pacman -Syy
-   pacman -S --noconfirm wget
-   _find_mirrorlist
-   _find_keyring
-   # Switch Mirrors to Local server for faster image creation
-   # and low bandwidth usage
-   if $LOCAL ; then
-        _switch_mirrors_local
-        pacman -Syy
-   fi
+#   pacman -S --noconfirm wget
 
    case $PLATFORM_NAME in
      OdroidN2) pacman -R --noconfirm linux-odroid-n2 uboot-odroid-n2
                pacman -Syu --noconfirm --needed linux-odroid linux-odroid-headers uboot-odroid-n2plus # odroid-n2-post-install
-               cp /home/alarm/n2-boot.ini /boot/boot.ini
+#               cp /home/alarm/n2-boot.ini /boot/boot.ini
                ;;
      RPi64)    pacman -R --noconfirm linux-aarch64 uboot-raspberrypi
                pacman -Syu --noconfirm --needed linux-rpi raspberrypi-bootloader raspberrypi-firmware
@@ -197,16 +83,9 @@ Main() {
                ;;
    esac
 
-   case $OPSYSTEM in
-      Desktop) pacman -S --noconfirm --needed eos-packagelist
-              _base_addons ;;
-   esac
-
-   if $LOCAL ; then
-       # Switch Mirrors back to original
-       _switch_mirrors_back
-   fi
-
+   pacman -S --noconfirm --needed - < /home/alarm/server-addons
+   mkdir -p /etc/samba
+   cp /home/alarm/smb.conf /etc/samba/
    _finish_up
 }  # end of Main
 
--- build-server-image-eos.sh
+++ build-server-image-eos.sh
@@ -19,22 +19,18 @@ _partition_RPi4() {
 }
 
 _copy_stuff_for_chroot() {
-    cp build-image-chroot.sh MP2/root/
-#   cp config_script.sh MP2/root/
-#   cp -r configs/ MP2/home/alarm/
+    cp build-server-image-chroot.sh /mnt/root/
+    cp eos-ARM-server-config.sh /mnt/root/
+    cp smb.conf /mnt/home/alarm
+    cp server-addons /mnt/home/alarm
+    cp config-server.service /mnt/home/alarm/
     case $PLATFORM in
-      RPi64)    cp rpi4-config.txt MP2/home/alarm/ ;;
-      OdroidN2) cp n2-boot.ini MP2/home/alarm ;;
+      RPi64)    cp rpi4-config.txt /mnt/home/alarm/ ;;
+      OdroidN2) cp n2-boot.ini /mnt/home/alarm ;;
     esac
     printf "$PLATFORM\n" > platformname
-    printf "$LOCAL\n" > mirrors
-    printf "$OPSYS\n" > operatingsystem
-    cp platformname MP2/root/
-    cp mirrors MP2/root/
-    cp operatingsystem MP2/root
+    cp platformname /mnt/root/
     rm platformname
-    rm mirrors
-    rm operatingsystem
 }
 
 _install_OdroidN2_image() {
@@ -43,9 +39,8 @@ _install_OdroidN2_image() {
         wget http://os.archlinuxarm.org/os/ArchLinuxARM-odroid-n2-latest.tar.gz
     fi
     printf "\n\n${CYAN}Untarring the image...might take a few minutes.${NC}\n"
-    bsdtar -xpf ArchLinuxARM-odroid-n2-latest.tar.gz -C MP2
-    # mv MP2/boot/* MP1
-    dd if=MP2/boot/u-boot.bin of=$DEVICENAME conv=fsync,notrunc bs=512 seek=1
+    bsdtar -xpf ArchLinuxARM-odroid-n2-latest.tar.gz -C /mnt
+    dd if=/mnt/boot/u-boot.bin of=$DEVICENAME conv=fsync,notrunc bs=512 seek=1
     _copy_stuff_for_chroot
 }   # End of function _install_OdroidN2_image
 
@@ -56,12 +51,11 @@ _install_RPi4_image() {
         wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
     fi
     printf "\n\n${CYAN}Untarring the image...may take a few minutes.${NC}\n"
-    bsdtar -xpf ArchLinuxARM-rpi-aarch64-latest.tar.gz -C MP2
+    bsdtar -xpf ArchLinuxARM-rpi-aarch64-latest.tar.gz -C /mnt
     printf "\n\n${CYAN}syncing files...may take a few minutes.${NC}\n"
     sync
-    # mv MP2/boot/* MP1
     _copy_stuff_for_chroot
-    sed -i 's/mmcblk0/mmcblk1/' MP2/etc/fstab
+    sed -i 's/mmcblk0/mmcblk1/' /mnt/etc/fstab
 }  # End of function _install_RPi4_image
 
 _partition_format_mount() {
@@ -76,9 +70,8 @@ _partition_format_mount() {
    ((DEVICESIZE=$DEVICESIZE/1048576))
    ((DEVICESIZE=$DEVICESIZE-1))  # for some reason, necessary for USB thumb drives
    printf "\n${CYAN}Partitioning storage device $DEVICENAME...${NC}\n"
-   printf "\ndevicename = $DEVICENAME     devicesize = $DEVICESIZE\n" >> /root/enosARM.log
    # umount partitions before partitioning and formatting
-   lsblk $DEVICENAME -o MOUNTPOINT | grep /run/media > mounts
+   lsblk $DEVICENAME -o MOUNTPOINT | grep "/" > mounts
    count=$(wc -l mounts | awk '{print $1}')
    if [[ "$count" -gt "0" ]]
    then
@@ -86,16 +79,16 @@ _partition_format_mount() {
       do
          u=$(awk -v "x=$i" 'NR==x' mounts)
          umount $u
+#         rm -rf $u
       done
    fi
    rm mounts
+
    case $PLATFORM in   
       RPi64)    _partition_RPi4 ;;
       OdroidN2) _partition_OdroidN2 ;;
-      Pinebook) _partition_Pinebook ;;
    esac
   
-   printf "\npartition name = $DEVICENAME\n\n" >> /root/enosARM.log
    printf "\n${CYAN}Formatting storage device $DEVICENAME...${NC}\n"
    printf "\n${CYAN}If \"/dev/sdx contains a ext4 file system Labelled XXXX\" or similar appears, Enter: y${NC}\n\n\n"
 
@@ -103,17 +96,14 @@ _partition_format_mount() {
    then
       DEVICENAME=$DEVICENAME"p"
    fi
-   
-   PARTNAME1=$DEVICENAME"1"
-   mkfs.fat $PARTNAME1   2>> /root/enosARM.log
-   PARTNAME2=$DEVICENAME"2"
-   mkfs.ext4 -F $PARTNAME2   2>> /root/enosARM.log
-   # mkdir MP1 MP2
-   mkdir MP2
-   mount $PARTNAME2 MP2
-   mkdir MP2/boot
-   mount $PARTNAME1 MP2/boot
 
+   PARTNAME1=$DEVICENAME"1"
+   mkfs.fat $PARTNAME1
+   PARTNAME2=$DEVICENAME"2"
+   mkfs.ext4 $PARTNAME2
+   mount $PARTNAME2 /mnt
+   mkdir /mnt/boot
+   mount $PARTNAME1 /mnt/boot
 } # end of function _partition_format_mount
 
 _check_if_root() {
@@ -130,36 +120,30 @@ _check_if_root() {
           exit
        fi
     fi
-    if [[ "$SUDO_USER" == "" ]]; then     
-         USERNAME=$USER
-    else
-         USERNAME=$SUDO_USER  
-    fi
 }  # end of function _check_if_root
 
 
 
 _arch_chroot(){
-    arch-chroot MP2 /root/build-image-chroot.sh
-    # arch-chroot MP2 /root/config_script.sh
+    arch-chroot /mnt /root/build-server-image-chroot.sh
 }
 
 _create_image(){
     case $PLATFORM in
-       OdroidN2)# time bsdtar --use-compress-program=zstdmt -cf /home/$USERNAME/pudges-place/server-images/enosARM-server-odroid-n2-latest.tar.zst *
-          time bsdtar -cf - * | zstd -z --rsyncable -10 -T0 -of /home/$USERNAME/pudges-place/server-images/enosARM-server-odroid-n2-latest.tar.zst
+       OdroidN2)# time bsdtar --use-compress-program=zstdmt -cf $IMAGEDIR/enosARM-server-odroid-n2-latest.tar.zst *
+          time bsdtar -cf - * | zstd -z --rsyncable -10 -T0 -of /$IMAGEDIR/enosARM-server-odroid-n2-latest.tar.zst
           printf "\n\nbsdtar is finished creating the image.\nand will calculate a sha512sum\n\n"
           cd ..
           dir=$(pwd)
-          cd /home/$USERNAME/pudges-place/server-images/
+          cd $IMAGEDIR
           sha512sum enosARM-server-odroid-n2-latest.tar.zst > enosARM-server-odroid-n2-latest.tar.zst.sha512sum
           cd $dir ;;
-       RPi64) # time bsdtar --use-compress-program=zstdmt -cf /home/$USERNAME/pudges-place/server-images/enosARM-server-rpi-latest.tar.zst *
-          time bsdtar -cf - * | zstd -z --rsyncable -10 -T0 -of /home/$USERNAME/pudges-place/server-images/enosARM-server-rpi-latest.tar.zst
+       RPi64) # time bsdtar --use-compress-program=zstdmt -cf $IMAGEDIR/enosARM-server-rpi-latest.tar.zst *
+          time bsdtar -cf - * | zstd -z --rsyncable -10 -T0 -of $IMAGEDIR/enosARM-server-rpi-latest.tar.zst
           printf "\n\nbsdtar is finished creating the image.\nand will calculate a sha512sum\n\n"
           cd ..
           dir=$(pwd)
-          cd /home/$USERNAME/pudges-place/server-images/
+          cd $IMAGEDIR
           sha512sum enosARM-server-rpi-latest.tar.zst > enosARM-server-rpi-latest.tar.zst.sha512sum
           cd $dir ;;
     esac
@@ -174,18 +158,16 @@ _help() {
    printf "These options are required\n"
    printf " -d  enter device name for ex: sda\n"
    printf " -p  enter platform: rpi or odn\n"
-   printf " -o  enter OS type: d or s (d=dekstop s=server)\n"
    printf "These options are optional\n"
    printf " -i  download base image: (y) or n\n"
    printf " -c  create image: (y) or n\n"
-   printf " -l  use local pacman mirrors: y or (n)\n"
-   printf "example: sudo ./build-image-eos -d sda -p rpi -o s -i y -c y -l n\n"
-   printf "Ensure that the directory /home/\$username/pudges-place/server-images exists\n\n"
+   printf "example: sudo ./build-server-image-eos.sh -d sda -p rpi -i y -c y\n"
+   printf "Ensure that the directory $IMAGEDIR exists\n\n"
 }
 
 _read_options() {
     # Available options
-    opt=":d:p:o:i:c:l:h:"
+    opt=":d:p:i:c:h:"
     local OPTIND
 
     if [[ ! $@ =~ ^\-.+ ]]
@@ -203,17 +185,11 @@ _read_options() {
         p)
           PLAT="${OPTARG}"
           ;;
-        o)
-          OS="${OPTARG}"
-          ;;
         i)
           DOWN="${OPTARG}"
           ;;
         c)
           CRE="${OPTARG}"
-          ;;
-        l)
-          LOC="${OPTARG}"
           ;;
         \?)
           echo "Option -${OPTARG} is not valid, aborting"
@@ -239,11 +215,6 @@ case $PLAT in
      *) PLAT1=true;;
 esac
 
-case $OS in
-     d) OPSYS="Desktop" ;;
-     s) OPSYS="Server" ;;
-esac
-
 case $DOWN in
      y) DOWNLOAD=true ;;
      n) DOWNLOAD=false ;;
@@ -255,14 +226,6 @@ case $CRE in
      n) CREATE=false ;;
      *) CREATE=true ;;
 esac
-
-case $LOC in
-     y) LOCAL=true ;;
-     n) LOCAL=false ;;
-     *) LOCAL=false ;;
-esac
-
-
 }
 
 #################################################
@@ -274,17 +237,17 @@ Main() {
     PLAT=""
     PLATFORM=" "     # e.g. OdroidN2, RPi4b, etc.
     DEVICENAME=" "   # storage device name e.g. /dev/sda
-    OPSYS=" "        # operating system e.g. Desktop, Server
+    IMAGEDIR="/home/don/pudges-place/server-images"
+#   OPSYS=" "        # operating system e.g. Desktop, Server
     DEVICESIZE="1"
     PARTNAME1=" "
     PARTNAME2=" "
-    USERNAME=" "
     DEVICETYPE=" "
     DOWN=" "
     DOWNLOAD=" "
     CREATE=" "
     LOC=" "
-    LOCAL=" "
+#   LOCAL=" "
     
     # Declare color variables
     GREEN='\033[0;32m'
@@ -307,13 +270,25 @@ Main() {
 
     if $CREATE ; then
         printf "\n\n${CYAN}Creating Image${NC}\n\n"
-        cd MP2
+        case $PLATFORM in
+            RPi64) imagetype="rpi" ;;
+            OdroidN2) imagetype="odroid-n2";;
+        esac
+        if [ -f "$IMAGEDIR/enosARM-server-$imagetype-latest.tar.zst" ]; then
+           rm $IMAGEDIR/enosARM-server-$imagetype-latest.tar.zst $IMAGEDIR/enosARM-server-$imagetype-latest.tar.zst.sha512sum
+       fi
+
+        printf "\nCheck for existing images\n\n"
+        ls $IMAGEDIR
+        printf "\n\n"
+
+        cd /mnt
         _create_image
         printf "\n\n${CYAN}Created Image${NC}\n\n"
     fi
 
-    umount MP2/boot MP2
-    rm -rf MP2
+    umount /mnt/boot /mnt
+#    rm -rf MP2
     # rm ArchLinuxARM*
 
     exit
